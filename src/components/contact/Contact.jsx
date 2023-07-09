3cd15804a78dd4264b36fcb4418128eab6420ba1
shojol021 <shojol021@gmail.com>
2023-07-09 23:09:41 +0600

    added react icons

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,9 @@
-
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
 
 const Contact = () => {
     return (
-        <div className="bg-teal-400">
+        <div id='contact' className="bg-teal-400">
             <h1 data-aos="fade-up" className='text-center font-bold text-6xl mb-10 mt-20 pt-10'>Contact</h1>
 
             <footer className="footer p-10">
@@ -12,8 +13,8 @@ const Contact = () => {
                     <p>+8801683180668</p>
                     <span className="footer-title mt-12">Social</span>
                     <div className="grid grid-flow-col gap-4">
-                        <a href="https://www.linkedin.com/in/shojol021">Linkdin</a>
-                        <a href="https://github.com/shojol021">Github</a>
+                        <a href="https://www.linkedin.com/in/shojol021"><FaLinkedin size='2rem'></FaLinkedin></a>
+                        <a href="https://github.com/shojol021"><FaGithub size='2rem'></FaGithub></a>
                     </div>
                 </div>
                 <div className="">
