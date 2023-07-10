c8bebf35659edd502a84e610799c577087067bcc
shojol021 <shojol021@gmail.com>
2023-07-10 23:59:57 +0600

    added react icons

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -2,13 +2,18 @@ import './banner.css'
 import image from '../../assets/shojol.jpg'
 
 const Banner = () => {
+
+    const handleResumeClick = () => {
+        window.open('https://drive.google.com/uc?export=download&id=15h0F312tF2SVF2ZDV5I_wXjnsvZEfYoC', '_blank');
+    };
+    
     return (
         <div>
             <div className="bg-cover gradient-bg bg-center h-screen flex justify-center items-center">
                 <div data-aos="fade-up" data-aos-delay="300">
                     <h2 className="text-white text-4xl text-start  py-4">Hi, I am</h2>
                     <h1 className="text-white text-6xl font-bold ">Adnan Hossain Shojol</h1>
-                    <a target='_blank' rel="noopener noreferrer" href="https://drive.google.com/file/d/15h0F312tF2SVF2ZDV5I_wXjnsvZEfYoC/view?usp=sharing"><button className='btn btn-primary mt-5'>Checkout Resume</button></a>
+                    <button onClick={handleResumeClick} className='btn btn-primary mt-5'>Checkout Resume</button>
                 </div>
                 <div data-aos="fade-left" data-aos-delay="400" className='w-1/4 ms-6'>
                     <img className='h-30' src={image} alt="" />
